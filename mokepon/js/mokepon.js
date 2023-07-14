ed4ab76e5c1d8f3dad1ec0abcb44cdc5f44db896
Alexis Calderon <alexnike.1993@gmail.com>
2023-07-14 15:09:46 -0400

    Escuchando eventos con JavaScript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,7 +1,19 @@
-
-function seleccionarMascotaJugador(){
-
+function iniciarHtml(){
+    let botonMascotaJugador = document.getElementById('boton_mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton_mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+    let radio1 = document.getElementById('Hipodoge');
+    let radio2 = document.getElementById('Capipepo');
+    let radio3 = document.getElementById('Ratigueya');
+    if(radio1.checked){
+        
+    }else if(radio2.checked){
+
+    }else if(radio3.checked){
+
+    }
+}
+
+window.addEventListener('load', iniciarHtml)
