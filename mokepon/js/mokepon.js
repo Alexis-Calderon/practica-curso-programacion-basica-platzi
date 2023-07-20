7428bac40b44000f9e542ac3cc009bab83e7f8f2
Alexis Calderon <alexnike.1993@gmail.com>
2023-07-20 15:03:18 -0400

    Mapa responsive

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -40,17 +40,28 @@ let intervalo
 let mapaBackground = new Image()
 mapaBackground.src = "./assets/mokemap.png"
 let mascotaJugadoObjeto
+let alturaQueBuscamos
+let anchoDelMapa = window.innerWidth -20
+const anchoMaximoDelMapa = 350
+
+if (anchoDelMapa > anchoMaximoDelMapa) {
+  anchoDelMapa = anchoMaximoDelMapa -20
+}
+
+alturaQueBuscamos = anchoDelMapa * 600 / 800
+mapa.width = anchoDelMapa
+mapa.height = alturaQueBuscamos
 
 class Mokepon {
-  constructor(nombre, foto, vida, fotoMapa, x = 10, y = 10) {
+  constructor(nombre, foto, vida, fotoMapa) {
     this.nombre = nombre;
     this.foto = foto;
     this.vida = vida;
     this.ataques = [];
-    this.x = x;
-    this.y = y;
     this.ancho = 40;
     this.alto = 40;
+    this.x = aleatorio(0, mapa.width - this.ancho);
+    this.y = aleatorio(0, mapa.height - this.alto);
     this.mapaFoto = new Image();
     this.mapaFoto.src = fotoMapa;
     this.velocidadX = 0;
@@ -85,25 +96,19 @@ let hipodogeEnemigo = new Mokepon(
   "Hipodoge",
   "./assets/mokepons_mokepon_hipodoge_attack.png",
   5,
-  "./assets/hipodoge.png",
-  80,
-  120
+  "./assets/hipodoge.png"
 );
 let capipepoEnemigo = new Mokepon(
   "Capipepo",
   "./assets/mokepons_mokepon_capipepo_attack.png",
   5,
-  "./assets/capipepo.png",
-  150,
-  95
+  "./assets/capipepo.png"
 );
-let ratigueyaEnemigo = new Mokepon(
+let ratigueyaEnemigo = new Mokepon( 
   "Ratigueya",
   "./assets/mokepons_mokepon_ratigueya_attack.png",
   5,
-  './assets/ratigueya.png',
-  200,
-  190
+  './assets/ratigueya.png'
 );
 
 hipodoge.ataques.push(
@@ -397,8 +402,6 @@ function sePresionoUnaTecla(event) {
 }
 
 function iniciarMapa() {
-  mapa.width = 800
-  mapa.height = 600
   mascotaJugadoObjeto = obtenerObjetoMascota(mascotaJugador)
   intervalo = setInterval(pintarCanvas, 50);
   window.addEventListener("keydown", sePresionoUnaTecla);
